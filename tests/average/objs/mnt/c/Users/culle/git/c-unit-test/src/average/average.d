7749081711888cb3622352cfeec33aad0b7c6c17
Robert Culley <rculley@lyrasystems.com>
2024-01-15 10:45:07 -0500

    Created unit test framework

---
+++ tests/average/objs/mnt/c/Users/culle/git/c-unit-test/src/average/average.d
@@ -0,0 +1,17 @@
+objs//mnt/c/Users/culle/git/c-unit-test/src/average/average.o: \
+ /mnt/c/Users/culle/git/c-unit-test/src/average/average.c \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h \
+ /mnt/c/Users/culle/git/c-unit-test/src/average/average.h
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h:
+
+/mnt/c/Users/culle/git/c-unit-test/src/average/average.h:
---
+++ tests/average/objs/mnt/c/Users/culle/git/c-unit-test/tests/average/test.d
@@ -0,0 +1,47 @@
+objs//mnt/c/Users/culle/git/c-unit-test/tests/average/test.o: \
+ /mnt/c/Users/culle/git/c-unit-test/tests/average/test.cpp \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestHarness.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/Utest.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/SimpleString.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/UtestMacros.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestResult.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestFailure.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestPlugin.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h \
+ /mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ /mnt/c/Users/culle/git/c-unit-test/src/average/average.h
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestHarness.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/Utest.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/SimpleString.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/UtestMacros.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestResult.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestFailure.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/TestPlugin.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakWarningPlugin.h:
+
+/mnt/c/Users/culle/git/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+
+/mnt/c/Users/culle/git/c-unit-test/src/average/average.h:
